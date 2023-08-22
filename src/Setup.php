9e78fe23073a797cd8b2ed588cca9a31e3de614e
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2023-08-22 22:41:46 +0600

    Try to get input from user when they install package

--- src/Setup.php
+++ src/Setup.php
@@ -2,12 +2,17 @@
 
 namespace Centerpoint\Reader;
 
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
 class Setup
 {
-    public static function run()
+    public static function run(Event $event)
     {
-        $input = readline("Enter some input: ");
-        echo "You entered: $input\n";
+        // $input = readline("Enter some input: ");
+        // echo "You entered: $input\n";
+        $composer = $event->getComposer();
+        var_dump($composer);
     }
 }
 
