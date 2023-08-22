2f22195771e87cf222b62fb32de2560686f0dc1e
Muhammad Sanwarul Islam <muhammad.sanwarul94@gmail.com>
2023-08-22 15:37:45 +0600

    Remove postInstall function

--- src/ReaderServiceProvider.php
+++ src/ReaderServiceProvider.php
@@ -5,17 +5,6 @@ use Illuminate\Support\ServiceProvider;
 
 class ReaderServiceProvider extends ServiceProvider
 {
-    public static function postInstall()
-    {
-        $laravelVersion = app()->version();
-        $installedPackages = collect(json_decode(shell_exec('composer show --all -f json')))
-            ->pluck('version', 'name');
-        
-        // Log the gathered information
-        \Log::info("Reader version: " . $laravelVersion);
-        \Log::info("Installed packages: " . json_encode($installedPackages));
-    }
-
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ .'/routes/web.php');
